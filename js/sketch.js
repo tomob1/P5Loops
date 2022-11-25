acdd4a5e5fb4eb035fb11097dd676b3b8305bf5c
Tom O'Brien <116882874+tomob1@users.noreply.github.com>
2022-11-25 12:43:35 +0000

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -2,16 +2,20 @@ function setup() {
     var canvas = createCanvas(800, 800);
     canvas.parent("p5container");
 
-    for(var i = 0; i < 10; i++){
-        console.log('I want ice cream')
+    // for(var i = 0; i < 10; i++){
+    //     console.log('I want' + i + 'Ice creams')
 
-    }
+    // }
 
 }
 
 function draw() {
     background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    stroke(255);
+
+    for (var lineX = 75; lineX <= 225; lineX += 75) {
+        line(lineX, 0, lineX, height);
+    }
+
 }
